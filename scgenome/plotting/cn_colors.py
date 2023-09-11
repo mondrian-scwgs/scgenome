9f247ead25d3d68e10f96d21279b77b6c825b7d9
Diljot Grewal <diljot.grewal@gmail.com>
2023-09-11 09:08:44 -0700

    refactored cn colors class

--- scgenome/plotting/cn_colors.py
+++ scgenome/plotting/cn_colors.py
@@ -1,33 +1,130 @@
+"""
+input: fields cn, copy state giemsa etc
+       dict of colors hex
+       number of levels
+        cmap name
+return:
+        rgb color ref
+        hex color ref
+        colormap
+
+
+
+
+"""
+
+from collections import defaultdict
+
+import matplotlib
+import matplotlib.cm
+import matplotlib.cm
 import numpy as np
-
-from matplotlib.patches import Patch
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import to_rgb, to_hex
 from numpy import ndarray
+from matplotlib.patches import Patch
 
 
-color_reference = {
-    0:'#3182BD',
-    1:'#9ECAE1',
-    2:'#CCCCCC',
-    3:'#FDCC8A',
-    4:'#FC8D59',
-    5:'#E34A33',
-    6:'#B30000',
-    7:'#980043',
-    8:'#DD1C77',
-    9:'#DF65B0',
-    10:'#C994C7',
-    11:'#D4B9DA',
-}
+class Colors(object):
+    def __init__(
+            self,
+            field_name=None,
+            levels=None,
+    ):
+
+        if field_name is not None:
+            if field_name.lower() in ('copy', 'state', 'cn'):
+                self.hex_color_reference = self.cn_color_reference
+            elif field_name.lower() in ('giemsa', 'cyto_band_giemsa_stain'):
+                self.hex_color_reference = self.cyto_band_giemsa_stain_hex_color_reference
+            else:
+                self.hex_color_reference = None
+
+        elif levels is not None:
+            cmap = self.infer_colormap_from_levels(len(levels))
+            cmap = self.load_preset_colormap(cmap)
+            self.hex_color_reference = dict(zip(levels, list(cmap(np.linspace(0, 1, len(levels))))))
+
+        self.rgb_color_reference = self.translate_hex_to_rgb(self.hex_color_reference)
+
+    @property
+    def cn_color_reference(self):
+        color_reference = {
+            0: '#3182BD',
+            1: '#9ECAE1',
+            2: '#CCCCCC',
+            3: '#FDCC8A',
+            4: '#FC8D59',
+            5: '#E34A33',
+            6: '#B30000',
+            7: '#980043',
+            8: '#DD1C77',
+            9: '#DF65B0',
+            10: '#C994C7',
+            11: '#D4B9DA',
+        }
+
+        color_reference = defaultdict(lambda: '#D4B9DA', color_reference)
+
+        return color_reference
+
+    @property
+    def cyto_band_giemsa_stain_hex_color_reference(self):
+        """
+         Adapted from: https://github.com/bernatgel/karyoploteR/blob/master/R/color.R
+        """
+        color_reference = {
+            'gneg': '#FFFFFF',
+            'gpos25': '#C8C8C8',
+            # 'gpos33': '#D2D2D2',
+            'gpos50': '#C8C8C8',
+            # 'gpos66': '#A0A0A0',
+            'gpos75': '#828282',
+            'gpos100': '#000000',
+            'gpos': '#000000',
+            'stalk': '#647FA4',  # repetitive areas
+            'acen': '#D92F27',  # centromeres
+            'gvar': '#DCACAC',  # previously '#DCDCDC'
+        }
+        return color_reference
+
+    def infer_colormap_from_levels(self, num_levels):
+        if num_levels <= 10:
+            return 'tab10'
+        elif num_levels <= 20:
+            return 'tab20'
+        else:
+            return 'hsv'
+
+    def load_preset_colormap(self, palette):
+        matplotlib_cmaps = plt.colormaps()
+
+        if palette in matplotlib_cmaps:
+
+            palette = matplotlib.cm.get_cmap(palette)
+        else:
+            palette = sns.color_palette(palette, as_cmap=True)
+
+        return palette
+
+    def translate_hex_to_rgb(self, color_reference):
+        if color_reference is None:
+            return None
+
+        new_reference = {}
+        for k, v in color_reference.items():
+            new_reference[k] = to_rgb(v)
+
+        if isinstance(color_reference, defaultdict):
+            default_value = to_rgb(color_reference['GETDEFAULTCOLOR'])
+            new_reference = defaultdict(lambda: default_value, new_reference)
+
+        return new_reference
 
 
-def hex_to_rgb(h):
-    if h is None:
-        return np.array((0, 0, 0), dtype=int)
-    h = h.lstrip('#')
-    return np.array(tuple(np.uint8(int(h[i:i+2], 16)) for i in (0, 2 ,4)), dtype=int)
-
-
-def map_cn_colors(X: ndarray) -> ndarray:
+def map_cn_colors_to_matrix(X: ndarray) -> ndarray:
     """ Create an array of colors from an array of copy number states
 
     Parameters
@@ -40,12 +137,28 @@ def map_cn_colors(X: ndarray) -> ndarray:
     ndarray
         colors with shape X.shape + (3,)
     """
-    X_colors = np.zeros(X.shape + (3,), dtype=int)
-    X_colors[X < 0, :] = 0
-    X_colors[X > max(color_reference.keys()), :] = max(color_reference.keys())
-    for state, hex in color_reference.items():
-        X_colors[X == state, :] = hex_to_rgb(hex)
-    return X_colors
+
+    color_reference = Colors(field_name='cn').rgb_color_reference
+
+    col = []
+
+    for idx in range(len(X)):
+        col.append(np.array([color_reference[v] for v in X[idx]]))
+
+    col = np.array(col)
+
+    return col
+
+
+def map_colormap_to_levels(levels, colors=None):
+    if colors is not None:
+        try:
+            color_ref = Colors(field_name=colors).rgb_color_reference
+            return color_ref
+        except:
+            pass
+
+    return Colors(levels=levels).rgb_color_reference
 
 
 def cn_legend(ax, frameon=True, loc=2, bbox_to_anchor=(0., 1.), title='Copy Number'):
@@ -67,19 +180,21 @@ def cn_legend(ax, frameon=True, loc=2, bbox_to_anchor=(0., 1.), title='Copy Numb
     Legend
         legend object
     """
+
+    color_reference = Colors('cn').hex_color_reference
+
     states = []
     patches = []
     for s, h in color_reference.items():
         states.append(s)
         patches.append(Patch(facecolor=h, edgecolor=h))
 
-    ncol = min(3, int(len(states)**(1/2)))
+    ncol = min(3, int(len(states) ** (1 / 2)))
 
     legend = ax.legend(patches, states, ncol=ncol,
-        frameon=frameon, loc=loc, bbox_to_anchor=bbox_to_anchor,
-        facecolor='white', edgecolor='white', fontsize='4',
-        title=title, title_fontsize='6')
+                       frameon=frameon, loc=loc, bbox_to_anchor=bbox_to_anchor,
+                       facecolor='white', edgecolor='white', fontsize='4',
+                       title=title, title_fontsize='6')
     legend.set_zorder(level=200)
 
     return legend
-
