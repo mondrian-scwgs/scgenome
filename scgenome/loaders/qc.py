021559000c0fdc193cf9c3a58e748539f1490162
eliko1991 <elih1991@hotmail.com>
2024-09-25 15:38:10 -0400

    add missing colon

--- scgenome/loaders/qc.py
+++ scgenome/loaders/qc.py
@@ -84,7 +84,7 @@ def load_qc_results(
 
         results_tables.update(hmmcopy_results_tables)
 
-    elif framework == 'mondrian'
+    elif framework == 'mondrian':
         alignment_results_dir = scgenome.loaders.utils.find_results_directory(
             alignment_results_dir, 'alignment')
 
